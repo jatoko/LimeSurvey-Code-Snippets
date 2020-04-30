3d86432a5153433d440ce3ac13e474d8928a4cc6
jatoko <17083784+jatoko@users.noreply.github.com>
2020-04-30 18:18:14 +0200

    Update endpoint_named_scale.js

--- 5-point-choice-endpoint-named-scale/endpoint_named_scale.js
+++ 5-point-choice-endpoint-named-scale/endpoint_named_scale.js
@@ -1,15 +1,40 @@
-var scaleNameLeft = "left side text";
-var scaleNameRight = "right side text";
+var scaleNameLeft = "left side text"; // e.g. "do not aggree"
+var scaleNameRight = "right side text"; // e.g. "aggree"
+
+// manual and custom text splitting for small screens
+if(screen.width < 900.00){
+    scaleNameLeft = "do not<br>aggree";
+    scaleNameRight = "completely<br>aggree";
+}
+
 $(document).ready(function() {
-    $('.ls-answers.answers-list.radio-list.list-unstyled.form-inline').each(function() {
-        if ($(this).find(".answer-item.radio-item.scale-name-left").size() == 0) {
-            $(this).prepend("<li class='answer-item radio-item scale-name-left'>" + scaleNameLeft + "</li>");
-        }
-        if ($(this).find(".answer-item.radio-item.scale-name-right").size() == 0) {
-            $(this).append("<li class='answer-item radio-item scale-name-right' style='padding-left: 0px; padding-right: 30px;'>" + scaleNameRight + "</li>");
-        }
-        if ($(this).find(".answer-item.radio-item.noanswer-item").size() > 0) {
-            $(this).find(".answer-item.radio-item.scale-name-right").after($(this).find(".answer-item.radio-item.noanswer-item"));
-        }
+    $('.ls-answers.answers-list.radio-list.list-unstyled.form-inline').each(function(){
+      if($( this ).find(".answer-item.radio-item.scale-name-left").size() == 0){
+          $( this ).prepend("<li class='answer-item radio-item scale-name-left'>" + scaleNameLeft + "</li>");
+      }
+      if($( this ).find(".answer-item.radio-item.scale-name-right").size() == 0){
+        $( this ).append("<li class='answer-item radio-item scale-name-right'>" + scaleNameRight + "</li>");
+      }
+      if($( this ).find(".answer-item.radio-item.noanswer-item").size() > 0){
+          $( this ).find(".answer-item.radio-item.scale-name-right").after($(this ).find(".answer-item.radio-item.noanswer-item"));
+      }
+      $( this ).find(".answer-item.radio-item.noanswer-item").wrap("<p></p>");
+    });
+
+    // Tag all elements:
+    $('label').each(function(){
+      $( this ).addClass("likert");
+    });
+    $('ul').each(function(){
+      $( this ).addClass("likert");
+    });
+    $('li').each(function(){
+      $( this ).addClass("likert");
+    });
+
+    // Move radio button element into label:
+    $('input[type="radio"]').each(function(){
+      $( this ).addClass("likert");
+      $( this ).appendTo('label[for=' + $(this).attr("id")+ ']');
     });
 });
